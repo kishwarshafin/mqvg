a78a326b49b98281d438db115270bbb8fcbd0daa
Kishwar <kishwar.shafin@gmail.com>
2017-05-03 00:21:57 -0700

    division by 0 error fixed

--- logisticRegression.py
+++ logisticRegression.py
@@ -75,7 +75,11 @@ class LogisticRegression:
 
         with open(outputDirectory+"/"+outputFile, 'w') as f:
             for idx, row in self.predictDataFrame.iterrows():
-                qValue = int(-10 * np.log10(float(predictions[index][0])))
+                try:
+                    p = np.log10(float(predictions[index][0]))
+                except ZeroDivisionError:
+                    p = 0
+                qValue = int(-10 * int(p))
                 if row[PREDICT_COLUMNS[3]]:
                     print(row[PREDICT_COLUMNS[0]], "\t", row[PREDICT_COLUMNS[1]], "\t",row[PREDICT_COLUMNS[2]], "\t", qValue,row[PREDICT_COLUMNS[3]], "\t", file=f)
                 else:
